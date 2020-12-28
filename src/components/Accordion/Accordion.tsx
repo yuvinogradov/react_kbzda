56bbbf8eb40ab18b5e8b09bd6ff156833be8b7a3
Yury Vinogradov <yuvinogradov@gmail.com>
2020-12-28 06:08:23 +0200

    08. to finish controlledOnOff

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,37 +1,46 @@
 import React from "react";
 
 type AccordionTitlePropsType = {
-  title: string;
+    title: string;
+    collapsed: boolean;
+    onClick: (collapsed: boolean) => void
+
 };
 
 type AccordionPropsType = {
-  title: string;
-  collapsed: boolean;
+    title: string;
+    collapsed: boolean;
+    onClick: (collapsed: boolean) => void
 };
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-  return <h3>--= {props.title} =--</h3>;
+    function clickHandler(){
+        alert('hello')
+        // debugger
+        props.onClick(!props.collapsed)
+    }
+    return <h3 onClick={()=>{props.onClick(!props.collapsed)}}>--= {props.title}  =--</h3>;
 }
 
 function AccordionBody() {
-  return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-    </ul>
-  );
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    );
 }
 
 function Accordion(props: AccordionPropsType) {
-  return (
-    <>
-      <AccordionTitle title={props.title} />
-      {/* {props.collapsed ? "" : <AccordionBody />} */}
-      {!props.collapsed && <AccordionBody />}
-    </>
-  );
+    return (
+        <>
+            <AccordionTitle title={props.title} onClick={props.onClick} collapsed={props.collapsed}/>
+            {/* {props.collapsed ? "" : <AccordionBody />} */}
+            {!props.collapsed && <AccordionBody/>}
+        </>
+    );
 }
 
 export default Accordion;
